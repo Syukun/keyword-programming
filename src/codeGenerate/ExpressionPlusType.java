c1046fe3a2d20ce400b00b94e6c51050ebac084a
Aochi <shuaochisoft@yahoo.co.jp>
2018-05-16 09:29:22 +0900

    05/14

---
+++ src/codeGenerate/ExpressionPlusType.java
@@ -0,0 +1,25 @@
+package codeGenerate;
+
+import java.util.Vector;
+
+public class ExpressionPlusType {
+	private String type;
+	private Vector<Expression> exps;
+	public ExpressionPlusType(String type,Vector<Expression> vector) {
+		this.setType(type);
+		this.setExps(exps);
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public Vector<Expression> getExps() {
+		return exps;
+	}
+	public void setExps(Vector<Expression> exps) {
+		this.exps = exps;
+	}
+
+}
